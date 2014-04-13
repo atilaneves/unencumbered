f41942f67a1f71cbfee2a1a072e5df51ab57f0c2
Atila Neves <aalvesne@cisco.com>
2014-04-13 11:18:44 +0200

    cucumber -f progress features/wired_on_tables.feature sort of passes

---
+++ features/wired_on_tables.feature
@@ -0,0 +1,6 @@
+Feature: High strung
+  Scenario: Wired and more
+    Given we're all:
+      | wired |
+      | high  |
+      | happy |
--- source/app.d
+++ source/app.d
@@ -1,7 +1,6 @@
 import vibe.d;
 import std.stdio;
-import core.time;
-
+import std.string;
 
 shared static this() {
     debug {
@@ -15,11 +14,40 @@ void accept(TCPConnection tcpConnection) {
         while(tcpConnection.connected) {
             auto bytes = new ubyte[tcpConnection.leastSize];
             tcpConnection.read(bytes);
-            debug writeln("\nLine:\n", cast(string)bytes, "\n");
+            debug writeln("Read ", bytes.length, " bytes");
+            handle(tcpConnection, (cast(string)bytes).strip());
         }
     });
 
     rtask.join();
 
     if(tcpConnection.connected) tcpConnection.close();
+}
+
+void send(TCPConnection tcpConnection, in string str) {
+    tcpConnection.write(str ~ "\r\n");
+}
+
+void handle(TCPConnection tcpConnection, in string request) {
+    debug writeln("request is length ", request.length);
+    debug writeln("\nLine:\n", request, "\n");
+    if(request == `["step_matches",{"name_to_match":"we're all wired"}]`) {
+        debug writeln("1");
+        tcpConnection.send(`["success",[]]`);
+    } else if(request == `["step_matches",{"name_to_match":"we're all:"}]`) {
+        debug writeln("2");
+        tcpConnection.write(`["success",[{"id":"1", "args":[{"val":"we're", "pos":0}]}]]` ~ "\r\n");
+    } else if(request == `["begin_scenario"]`) {
+        debug writeln("3");
+        tcpConnection.send(`["success"]`);
+    } else if(request == `["invoke",{"id":"1","args":["we're",[["wired"],["high"],["happy"]]]}]`) {
+        debug writeln("4");
+        tcpConnection.send(`["success"]`);
+    } else if(request == `["end_scenario"]`) {
+        debug writeln("5");
+        tcpConnection.send(`["success"]`);
+    } else  {
+        debug writeln("oops");
+        tcpConnection.send(`["",[]]`);
+    }
 }
