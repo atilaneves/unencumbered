62709650b751f94bc03d11c771f1e506ebfc0db2
Atila Neves <aalvesne@cisco.com>
2014-04-16 13:11:58 +0200

    Refactoring, unittest blocks and documentation for reflection.d

--- source/cucumber/reflection.d
+++ source/cucumber/reflection.d
@@ -7,24 +7,30 @@ import std.typetuple;
 
 
 private enum isMatchStruct(T) = is(T:Match!S, string S);
-static assert(isMatchStruct!(Match!""));
+
+unittest {
+    static assert(isMatchStruct!(Match!""));
+}
 
 private enum hasMatchUDA(alias T) = Filter!(isMatchStruct, __traits(getAttributes, T)).length > 0;
+
 private template matchToRegex(T: Match!S, string S) if(isMatchStruct!T) {
     enum matchToRegex = S;
 }
 
 enum getRegex(alias T) = matchToRegex!(Filter!(isMatchStruct, __traits(getAttributes, T))[0]);
 
-@Match!(r"^foo reg")
-private void foo() {
-    static assert(hasMatchUDA!(foo));
-    static assert(matchToRegex!(Match!"my regex") == "my regex");
-    static assert(getRegex!(foo) == "^foo reg");
-}
+unittest {
+    @Match!(r"^foo reg")
+    void foo() {
+        static assert(hasMatchUDA!(foo));
+        static assert(matchToRegex!(Match!"my regex") == "my regex");
+        static assert(getRegex!(foo) == "^foo reg");
+    }
 
-private void bar() {
-    static assert(!hasMatchUDA!(bar));
+    void bar() {
+        static assert(!hasMatchUDA!(bar));
+    }
 }
 
 struct CucumberStep {
@@ -32,21 +38,44 @@ struct CucumberStep {
     string regex;
 }
 
+
+/**
+ * Finds all steps in all modules. Modules are passed in as strings.
+ * Steps are found by using compile-time reflection to register
+ * all functions with the Match UDA attached to it and extracting
+ * the relevant regex from the Match UDA itself.
+ */
 auto findSteps(ModuleNames...)() if(allSatisfy!(isSomeString, (typeof(ModuleNames)))) {
     mixin(importModulesString!ModuleNames);
     CucumberStep steps[];
     foreach(mod; ModuleNames) {
         foreach(member; __traits(allMembers, mixin(mod))) {
-            static if(__traits(compiles, mixin(member)) && isSomeFunction!(mixin(member)) &&
-                      hasMatchUDA!(mixin(member))) {
-                enum reg = getRegex!(mixin(member));
-                mixin(`steps ~= CucumberStep(&` ~ member ~ `, r"` ~ reg ~ `");`);
+
+            enum compiles = __traits(compiles, mixin(member));
+
+            static if(compiles) {
+
+                enum isFunction = isSomeFunction!(mixin(member));
+                enum hasMatch = hasMatchUDA!(mixin(member));
+
+                static if(isFunction && hasMatch) {
+                    enum reg = getRegex!(mixin(member));
+                    mixin(`steps ~= CucumberStep(&` ~ member ~ `, r"` ~ reg ~ `");`);
+                    //e.g. steps ~= CucumberStep(&myfunc, r"foobar");
+                }
             }
         }
     }
+
     return steps;
 }
 
+
+/**
+ * Finds the match to a step string. Checks all steps and loops
+ * over to see which one has a matching regex. Steps are found
+ * at compile-time.
+ */
 void function() findMatch(ModuleNames...)(in string step_str) {
     enum steps = findSteps!ModuleNames;
     foreach(step; steps) {
