bb27e6c53220e655a2c42360d7296242e396ede4
Atila Neves <aalvesne@cisco.com>
2014-04-17 16:20:03 +0200

    Converted the calculator example to use the new type-safe compile-time trickery

--- tests/calculator/steps.d
+++ tests/calculator/steps.d
@@ -25,21 +25,21 @@ void calculatorReturns() {
 }
 
 @When!(`^the calculator adds up "?([0-9.]+)"? and "?([0-9.]+)"?$`)
-void whenAddsUp(in string a, in string b) {
+void whenAddsUp(in double a, in double b) {
     writeln("adding ", a, " to ", b);
-    calculator.add(a.to!double, b.to!double);
+    calculator.add(a, b);
 }
 
 @And!(r"^the calculator adds up ([0-9.]+) and ([0-9.]+)$")
-void andAddsUp(in string a, in string b) {
+void andAddsUp(in double a, in double b) {
     writeln("andAddsUp adding up ", a, " to ", b);
-    calculator.add(a.to!double, b.to!double);
+    calculator.add(a, b);
 }
 
 @When!(`the calculator adds up "([0-9.]+)", "([0-9.]+)" and "([0-9.]+)"`)
-void whenAdds3(in string a, in string b, in string c) {
+void whenAdds3(in double a, in double b, in double c) {
     writeln("whenAdds3 adding up ", a, ", ", b, " to ", c);
-    calculator.add(a.to!double, b.to!double, c.to!double);
+    calculator.add(a, b, c);
 }
 
 @But!(r"^the calculator does not return 3$")
