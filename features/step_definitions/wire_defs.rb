55d7a23577788d2deb350a2b827a08c74f4a604f
Atila Neves <aalvesne@cisco.com>
2014-04-17 18:09:34 +0200

    step definitions for the wire protocol

---
+++ features/step_definitions/wire_defs.rb
@@ -0,0 +1,4 @@
+Given(/^there is a wire server running on port (\d+) which understands the following protocol:$/) do |arg1, table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
